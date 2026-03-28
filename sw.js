e24c60f92f258e0eb5a2b64547b935ce44f0024e
Will Lachance <wlach@protonmail.com>
2026-03-28 10:35:09 -0400

    fixes

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "meditation-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/index.js",
+  "./sounds/bell.mp3",
+  "./img/bell-32.png",
+  "./img/bell-48.png",
+  "./img/bell-128.png",
+  "./img/bell-192.png",
+  "./img/bell-512.png",
+  "./img/bell-icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)),
+        ),
+      ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cached) => cached || fetch(event.request)),
+  );
+});
